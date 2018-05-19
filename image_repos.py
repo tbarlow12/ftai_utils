c5a566653332c32aa16cddc2b51db176839543f1
Ubuntu <tabarlow@tabarlow-vm.bcmpxq5zrf4e5ledl3prqp2akh.jx.internal.cloudapp.net>
2018-05-19 05:45:01 +0000

    basic functionality of getting images from blob and writing them to local

--- image_repos.py
+++ image_repos.py
@@ -1,34 +1,62 @@
 import os
+from os import listdir
+from os.path import isfile, join
+import threading
 from azure.storage.blob.blockblobservice import BlockBlobService
+import pdb
+
+def files_in_dir(dir):
+    return [f for f in listdir(dir) if isfile(join(dir, f))]
+
+def blob_name_to_local(blob_name):
+    return blob_name.replace('/', '-')
+
+def local_to_blob_name(local):
+    return local.replace('-', '/')
 
 class ImageRepo(object):
 
-    def download_blob(self):
-        pass
+    def download_image(self, blob_name, dest_path, container_name=None):
+        if container_name is None:
+            container_name = self.source_container
+        dest_path = dest_path.strip()
+        if len(dest_path) == 0:
+            dest_path = '.'
+        if dest_path[-1] != '/':
+            dest_path += '/'
+        local_path = dest_path + blob_name_to_local(blob_name)
+        print('Saving blob to path: ' + local_path)
+        self.blob_service.get_blob_to_path(
+            container_name, 
+            blob_name, 
+            local_path)
 
-    def download_source_images(self):
-        pass
+    def download_images(self, dest_path, container_name=None, tag=None):
+        if container_name is None:
+            container_name = self.source_container
+        if not os.path.exists(dest_path):
+            os.makedirs(dest_path)
+        tag_prefix = tag
+        if tag_prefix is not None and tag_prefix[-1] != '/':
+            tag_prefix += '/'
+        blobs_in_container = self.blob_service.list_blobs(container_name, prefix=tag_prefix)
+        threads = []
+        for blob in blobs_in_container:
+            t = threading.Thread(target=self.download_image, args=(blob.name, dest_path, container_name,))
+            threads.append(t)
+            t.start()
+        for thread in threads:
+            thread.join()
 
-    def upload_blob(self):
-        pass
-
-    def move_blob(self):
-        pass
-
-    def move_local(self):
-        pass
-
-    def delete_blob(self):
-        pass
-
-    def delete_local(self):
-        pass
+    def move_blob(self, blob_name, from_container, to_container):
+        blob_url = self.blob_service.make_blob_url(from_container, blob_name)
+        self.blob_service.copy_blob(to_container, blob_name, blob_url)
+        self.blob_service.delete_blob(from_container, blob_name)
 
     def __init__(self):
         account = os.environ['BLOB_ACCOUNT_NAME']
         key = os.environ['BLOB_ACCOUNT_KEY']
-        self.blob_service = BlockBlobService(account, key)
-    
+        self.blob_service = BlockBlobService(account, key)    
 
 class ImageNet(ImageRepo):
 
@@ -36,8 +64,26 @@ class ImageNet(ImageRepo):
 
 class UserImages(ImageRepo):
 
-    source_container = 'approved-images'
-    dest_container = 'processed-images'
+    source_container = 'approved-images-dev'
+    dest_container = 'processed-images-dev'
+
+    def image_processed(self, file_name):
+        self.move_blob(
+            local_to_blob_name(file_name), 
+            self.source_container, 
+            self.dest_container)
+
+    def dir_processed(self, dir):
+        files = files_in_dir(dir)
+        threads = []
+        for f in files:
+            blob_name = local_to_blob_name(f)
+            t = threading.Thread(target=self.image_processed, args=blob_name,)
+            threads.append(t)
+            t.start()
+        for thread in threads:
+            thread.join()
+
 
 class KaggleImages(ImageRepo):
 
